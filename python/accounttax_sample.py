a3d2e2182abb422d0b6dfa4b1cb179823044ce7e
Stevie Strickland <sstrickl@google.com>
2016-11-17 07:03:18 -0800

    Adding Accounttax and Shippingsettings examples to Python samples.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=139456170

---
+++ python/accounttax_sample.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+#
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Creates a sample accounttax object for the accounttax samples."""
+
+
+def create_accounttax_sample(account_id, **overwrites):
+  """Creates a sample accounttax resource object for the accounttax samples.
+
+  Args:
+      config: dictionary, Python version of config JSON
+      **overwrites: dictionary, a set of accounttax attributes to overwrite
+
+  Returns:
+      A new accounttax resource in dictionary form.
+  """
+  tax = {
+      'accountId': account_id,
+      'rules': [{
+          'country': 'US',
+          'locationId': 21167,
+          'useGlobalRate': True
+      }]
+  }
+  tax.update(overwrites)
+  return tax
---
+++ python/accounttax_update.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+#
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Updates the tax settings of the specified account."""
+
+import argparse
+import sys
+
+import accounttax_sample
+from oauth2client import client
+import shopping_common
+
+# Declare command-line flags.
+argparser = argparse.ArgumentParser(add_help=False)
+argparser.add_argument(
+    'account_id',
+    type=int,
+    help='The ID of the account for which to update information.')
+
+
+def main(argv):
+  # Authenticate and construct service.
+  service, config, flags = shopping_common.init(
+      argv, __doc__, parents=[argparser])
+  merchant_id = config['merchantId']
+  account_id = flags.account_id
+
+  if merchant_id != account_id:
+    shopping_common.check_mca(
+        config, True,
+        msg = 'Non-multi-client accounts can only set their own information.')
+
+  try:
+    settings = accounttax_sample.create_accounttax_sample(account_id)
+    status = service.accounttax().update(
+        merchantId=merchant_id, accountId=merchant_id,body=settings).execute()
+    print 'Account %s:' % status['accountId']
+    if 'rules' not in status:
+      print '- No tax settings, so no tax is charged.'
+    else:
+      print('- Found %d tax rules:' %
+            len(status['rules']))
+      for issue in status['rules']:
+        if 'ratePercent' in issue and issue['ratePercent']:
+          print('  - For %s in %s: %s%%' %
+                (issue['locationId'], issue['country'], issue['ratePercent']))
+        if 'useGlobalRate' in issue and issue['useGlobalRate']:
+          print('  - For %s in %s: using the global tax table rate.' %
+                (issue['locationId'], issue['country']))
+        if 'shippingTaxed' in issue and issue['shippingTaxed']:
+          print '   NOTE: Shipping charges are also taxed.'
+  except client.AccessTokenRefreshError:
+    print ('The credentials have been revoked or expired, please re-run the '
+           'application to re-authorize')
+
+if __name__ == '__main__':
+  main(sys.argv)
