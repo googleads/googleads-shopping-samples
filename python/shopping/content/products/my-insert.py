8f0e5db81acd13c22ee131ca2cd7bb563fd28921
zanthl2 <83887309+zanthl2@users.noreply.github.com>
2022-04-30 11:32:44 +0300

    Update and rename python/shopping/content/products/my-insert.py to Πύθων/ψώνια/περιεχόμενο/προϊόντα/my-insert.py

--- python/shopping/content/products/my-insert.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function
-import sys
-
-# The common module provides setup functionality used by the samples,
-# such as authentication and unique id generation.
-from shopping.content import common
